ac10e9b0edca61c6f405ec84e19b287fe009b164
Alexis Leandro Emanuel Segovia <aleleomeaseg@gmail.com>
2015-03-09 20:55:27 -0300

    Carpeta JS

---
+++ 04-templates-engines/js/json.js
@@ -0,0 +1,32 @@
+var data = {
+        
+        person:{
+        
+            firstName: "Alexis Leandro Emanuel", 
+            lastName: "Segovia",
+            birth: '21/03/1993',
+            gender: 'Male',
+            adress : 'Watt 5013 Bº Alte Brown, Córdoba Capital',
+            mail: 'aleleoemaseg@gmail.com',
+            phone: '3516132675'
+        },
+            jobTitle: "Web Developer",
+            location: 'Cordoba',
+            image: 'foto.jpg',
+            twitter: "@aleleoemaseg",
+            pastJobs:
+            [{
+                job: "technical sorporte at Allus (Telefonica Argentina)",
+                place: "Cordoba",
+                year: "2011-2014"
+            }],
+            Education:
+            [{
+                place: 'tecnicatura superior en programacion'
+            },
+            {
+                place: 'Universidad Tecnológica Nacional (UTN-FRC)'
+            }]
+        
+     
+};
