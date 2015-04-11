f179eaca8b8b07c46deac4d77f15e46dcd3c0b05
Alexis Leandro Emanuel Segovia <aleleomeaseg@gmail.com>
2015-04-11 10:40:14 -0300

    Create script.js

---
+++ 01-js-intro-and-jquery/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+  $("section").fadeIn(2000);
+      $("section").promise().done(function(){
+          $(".alias").focus();
+      }); 
+    $("#btn1").click(function(){   
+          var alias= $(".alias").val();
+          $.get("http://bootcamp.aws.af.cm/welcome/"+alias, function(data, status){
+          if (data.response){
+          $("section").html(data.response).css("background-color", "#F78181");          
+        }else{
+          $("section").html(data).css("background-color", "red");
+        }        
+        });
+    });
+      $("#btn2").click(function(){
+      $.get("http://localhost:3000/search?q=html5", function(data, status){
+        console.dir(data);
+            for (item in data['statuses']){
+              $('aside').append('<li><img src="' + data['statuses'][item]['user']['profile_image_url'] + '"/></li>');
+              $('aside').append('<li>' + data['statuses'][item]['user']['screen_name'] + '</li>');
+              $('aside').append('<li>' + data['statuses'][item]['text'] + '</li>');
+              $('aside').append('<li>' + data['statuses'][item]['user']['created_at'] + '</li>'+'<br>');              
+            }
+        });
+    });
+});
